b125ce043366a613f903a515ec9f4c2f793cd7be
fqchenxin@gmail.com <fqchenxin@gmail.com>
2018-11-11 02:00:40 +1100

    add login-auth function

--- src/App.js
+++ src/App.js
@@ -10,136 +10,105 @@ import Forum from "./pages/Forum/Forum";
 import Teacher from "./pages/Teacher/Teacher";
 import TakeCourse from "./pages/Course/TakeCourse";
 import TakeQuiz from "./pages/Course/TakeCourse";
+import AuthService from "./pages/LoginForm/AuthService";
+
+export const LoginContext = React.createContext({
+  setLogin: () => {}
+});
 
 class App extends Component {
   constructor() {
     super();
+    this.Auth = new AuthService();
+    this.setLogin = (val) => {
+      this.setState({
+        isLogin: val
+      });
+    }
     this.state = {
-      access_token: "start",
-      isLogin: false
+      isLogin: this.Auth.loggedIn(),
+      setLogin: this.setLogin,
     };
-    this.setToken = this.setToken.bind(this);
   }
-
-  setToken(val) {
-    this.setState({
-      access_token: val,
-      isLogin: true
-    });
-    console.log("set token");
-  }
-
+  
   render() {
-    console.log("islogin: " + this.state.isLogin);
     return (
-      <BrowserRouter>
-        <div>
-          <Switch>
-            {/* <Route path="/" component={DashBoard} exact /> */}
-            <Route
-              path="/"
-              exact
-              render={props => {
-                return this.state.isLogin ? (
-                  <Redirect to="/dashboard" />
-                ) : (
-                  <Redirect
-                    to={{
-                      pathname: "/login",
-                      state: { from: props.location }
-                    }}
-                  />
-                );
-              }}
-            />
+      <LoginContext.Provider value={ this.state }>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route
+                path="/"
+                exact
+                render={() => {
+                  return this.state.isLogin ? (
+                    <Redirect to="/dashboard" />
+                  ) : (
+                    <Redirect to="/login" />
+                  );
+                }}
+              />
+              <Route
+                path="/login"
+                render={props => {
+                  return this.state.isLogin ? (
+                    <Redirect to="/dashboard" />
+                  ) : (
+                    <LoginForm {...props} />
+                  );
+                }}
+              />
 
-            {/* <Route
-              path="/login"
-              render={props => {
-                return this.state.isLogin ? (
-                  <Redirect
-                    to={{
-                      pathname: "/dashboard",
-                      state: {
-                        isLogin: this.state.isLogin,
-                        from: props.location
-                      }
-                    }}
-                  />
-                ) : (
-                  <LoginForm {...props} setToken={this.setToken} />
-                );
-              }}
-            /> */}
-
-            <Route
-              path="/login"
-              islogin={this.state.isLogin}
-              render={({ islogin, ...props }) => {
-                return this.state.isLogin ? (
-                  <Redirect
-                    to={{ pathname: "/", state: { from: props.location } }}
-                  />
-                ) : (
-                  <LoginForm setToken={this.setToken} {...props} />
-                );
-              }}
-            />
-            {/* <Route
-              path="/login"
-              render={props => (
-                <LoginForm {...props} setToken={this.setToken} />
-              )}
-            /> */}
-            <PrivateRoute
-              isLogin={this.state.isLogin}
-              path="/course"
-              component={Course}
-            />
-            <PrivateRoute
-              isLogin={this.state.isLogin}
-              path="/takecourse"
-              component={TakeCourse}
-            />
-            <PrivateRoute
-              isLogin={this.state.isLogin}
-              path="/takequiz"
-              component={TakeQuiz}
-            />
-            <PrivateRoute
-              isLogin={this.state.isLogin}
-              path="/account"
-              component={Account}
-            />
-            <PrivateRoute
-              isLogin={this.state.isLogin}
-              path="/dashboard"
-              component={DashBoard}
-            />
-            <PrivateRoute
-              isLogin={this.state.isLogin}
-              path="/messages"
-              component={Messages}
-            />
-            <PrivateRoute
-              isLogin={this.state.isLogin}
-              path="/teacher"
-              component={Teacher}
-            />
-            <PrivateRoute
-              isLogin={this.state.isLogin}
-              path="/forum"
-              component={Forum}
-            />
-            <PrivateRoute
-              isLogin={this.state.isLogin}
-              path="/teacher"
-              component={Teacher}
-            />
-            <Route component={ErrorPage} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+              <PrivateRoute
+                isLogin={this.state.isLogin}
+                path="/course"
+                component={Course}
+              />
+              <PrivateRoute
+                isLogin={this.state.isLogin}
+                path="/takecourse"
+                component={TakeCourse}
+              />
+              <PrivateRoute
+                isLogin={this.state.isLogin}
+                path="/takequiz"
+                component={TakeQuiz}
+              />
+              <PrivateRoute
+                isLogin={this.state.isLogin}
+                path="/account"
+                component={Account}
+              />
+              <PrivateRoute
+                isLogin={this.state.isLogin}
+                path="/dashboard"
+                component={DashBoard}
+              />
+              <PrivateRoute
+                isLogin={this.state.isLogin}
+                path="/messages"
+                component={Messages}
+              />
+              <PrivateRoute
+                isLogin={this.state.isLogin}
+                path="/teacher"
+                component={Teacher}
+              />
+              <PrivateRoute
+                isLogin={this.state.isLogin}
+                path="/forum"
+                component={Forum}
+              />
+              <PrivateRoute
+                isLogin={this.state.isLogin}
+                path="/teacher"
+                component={Teacher}
+              />
+              <Route component={ErrorPage} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </LoginContext.Provider>
     );
   }
 }
